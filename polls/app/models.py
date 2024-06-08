3b3325aaf7e4633d40ce084caa76ff160ebf3819
maxf8185 <maxf8185@gmail.com>
2024-06-08 20:12:27 +0300

    my changes

--- polls/app/models.py
+++ polls/app/models.py
@@ -1,7 +1,39 @@
-from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db, login
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from typing import Optional
+
+
+user_poll = sa.Table(
+    'user_poll',
+    db.metadata,
+    sa.Column('user_id', sa.Integer, sa.ForeignKey('user.id'), primary_key=True),
+    sa.Column('poll_id', sa.Integer, sa.ForeignKey('poll.id'), primary_key=True)
+)
+
+
+class User(UserMixin, db.Model):
+    id: so.MappedColumn[int] = so.mapped_column(primary_key=True)
+    username: so.MappedColumn[str] = so.mapped_column(sa.String(60))
+    password_hash: so.MappedColumn[Optional[str]] = so.mapped_column(sa.String(60))
+    voted_polls: so.WriteOnlyMapped['Poll'] = so.relationship('Poll', secondary=user_poll, back_populates='voters')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return self.username
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
 
 
 class Category(db.Model):
@@ -13,12 +45,14 @@ class Category(db.Model):
     def __repr__(self):
         return f'Category: {self.name}'
 
+
 class Poll(db.Model):
     id: so.MappedColumn[int] = so.mapped_column(primary_key=True)
     topic: so.MappedColumn[str] = so.mapped_column(sa.String(100))
     options: so.WriteOnlyMapped['Option'] = so.relationship(back_populates='poll')
     category_id: so.MappedColumn[int] = so.mapped_column(sa.ForeignKey(Category.id))
     category: so.Mapped[Category] = so.relationship(back_populates='polls')
+    voters: so.WriteOnlyMapped[User] = so.relationship('User', secondary=user_poll, back_populates='voted_polls')
 
     def __repr__(self):
         return f'Poll: {self.topic}'
@@ -27,7 +61,7 @@ class Poll(db.Model):
 class Option(db.Model):
     id: so.MappedColumn[int] = so.mapped_column(primary_key=True)
     title: so.MappedColumn[str] = so.mapped_column(sa.String(100))
-    votes: so.MappedColumn[int]
+    votes: so.MappedColumn[int] = so.mapped_column(default=0)
     poll_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Poll.id))
     poll: so.Mapped[Poll] = so.relationship(back_populates='options')
 
--- polls/app/routes.py
+++ polls/app/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 import sqlalchemy as sa
+from flask_login import logout_user, current_user, login_required, login_user
 
 from app import app, db
-from app.models import Poll, Option, Category
+from app.models import Poll, Option, Category, User
 
 
 @app.route('/')
+@login_required
 def index():
     polls = db.session.scalars(sa.select(Poll)).all()
     categories = db.session.scalars(sa.select(Category)).all()
@@ -33,3 +35,68 @@ def category(category_id):
     category = db.session.scalar(sa.select(Category).where(Category.id == category_id))
     polls = db.session.scalars(category.polls.select()).all()
     return render_template('category.html', polls=polls)
+
+
+@app.route('/add-poll', methods=['GET', 'POST'])
+def add_poll():
+    categories = db.session.scalars(sa.select(Category)).all()
+    if request.method == 'POST':
+        poll_topic = request.form.get('poll_topic')
+        category_id = request.form.get('category')
+        # category = db.session.scalar(sa.select(Category).where(Category.id == int(category_id)))
+        category = db.session.get(Category, int(category_id))
+        poll = Poll(topic=poll_topic, category=category)
+        db.session.add(poll)
+        db.session.commit()
+        return redirect(url_for('add_option', poll_id=poll.id))
+    return render_template('add_poll.html', categories=categories)
+
+
+@app.route('/add-option/<int:poll_id>', methods=['GET', 'POST'])
+def add_option(poll_id):
+    poll = db.session.get(Poll, poll_id)
+    options = db.session.scalars(poll.options.select()).all()
+    if request.method == 'POST':
+        title = request.form.get('title')
+        option = Option(title=title, poll=poll)
+        db.session.add(option)
+        db.session.commit()
+        return redirect(url_for('add_option', poll_id=poll_id))
+    return render_template('add_option.html', poll=poll, options=options)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return '<h1>log out please</h1>'
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = db.session.scalar(sa.select(User).where(User.username == username))
+        if not user or not user.check_password(password):
+            return '<h1>User not valid</h1>'
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return '<h1>Logged out</h1>'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return '<h1>log out please</h1>'
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User(username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('login')
+    return render_template('registration.html')
