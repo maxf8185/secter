a202083ac7cd70f08e9bad7218fbcb223bfcf109
maxf8185 <maxf8185@gmail.com>
2024-06-16 23:14:19 +0300

    my changes

--- tours/app/models.py
+++ tours/app/models.py
@@ -49,3 +49,14 @@ class Tour(db.Model):
 
     def __repr__(self):
         return f'Tour: {self.title}'
+
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tour_id = db.Column(db.Integer, db.ForeignKey('tour.id'), nullable=False)
+    user_name = db.Column(db.String(100), nullable=False)
+    user_email = db.Column(db.String(120), nullable=False)
+    booking_date = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f'<Booking {self.id} for Tour {self.tour_id}>'
--- tours/app/routes.py
+++ tours/app/routes.py
@@ -3,10 +3,38 @@ import sqlalchemy as sa
 from flask_login import logout_user, current_user, login_required, login_user
 
 from app import app, db
-from .models import Tour, User
+from .models import Tour, User, Booking
 
 
 @app.route('/')
 def index():
     tours = db.session.scalars(sa.select(Tour)).all()
     return render_template('index.html', tours=tours)
+
+
+@app.route('/book', methods=['GET', 'POST'])
+def book_tour():
+    if request.method == 'POST':
+        tour_id = request.form['tour_id']
+        user_name = request.form['user_name']
+        user_email = request.form['user_email']
+        booking_date = request.form['booking_date']
+        num_people = request.form['num_people']
+
+        new_booking = Booking(tour_id=tour_id, user_name=user_name, user_email=user_email, booking_date=booking_date,
+                              num_people=num_people)
+        db.session.add(new_booking)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    else:
+        tours = Tour.query.all()
+        return render_template('book.html', tours=tours)
+
+
+@app.route('/bookings')
+def bookings():
+    bookings = Booking.query.all()
+    return render_template('bookings.html', bookings=bookings)
+
+
