d756a5c5245a024cbaab7d7bd9e187d5a63efee1
maxf8185 <maxf8185@gmail.com>
2024-08-15 19:01:44 +0300

    my first message

--- docker_example/hello.py
+++ docker_example/hello.py
@@ -1,1 +1,13 @@
-print('Hello world')
+from typing import Optional
+
+from flask import Flask
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db:5432/postgres'
+db = SQLAlchemy(app)
--- tours/config.py
+++ tours/config.py
@@ -5,7 +5,7 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '123456789'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'tours.db')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@postgres_tours:5432/postgres'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
